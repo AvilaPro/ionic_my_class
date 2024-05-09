14b78d6c49398b0eb7ba4575cbb7fb1e47a1c88d
avilapro <mejiasavilaeduardo@gmail.com>
2024-05-09 17:08:04 -0400

    clase 3 ionic 2 - uso de la camara

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,20 +2,7 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
-  },
-  {
-    path: 'perfil',
-    loadComponent: () => import('./perfil/perfil.page').then( m => m.PerfilPage)
-  },
-  {
-    path: 'login',
-    loadComponent: () => import('./login/login.page').then( m => m.LoginPage)
-  },
-  {
     path: '',
-    redirectTo: 'login',
-    pathMatch: 'full',
+    loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
   },
 ];
---
+++ src/app/interface/foto-usuario.ts
@@ -0,0 +1,4 @@
+export interface FotoUsuario {
+  filePath: string,
+  webviewPath: string
+}
