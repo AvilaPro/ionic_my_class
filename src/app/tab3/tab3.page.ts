61bba022d2ee399beaa0bd42bd2dd510c30d6f4a
avilapro <mejiasavilaeduardo@gmail.com>
2024-05-16 17:10:12 -0400

    clase 4 - Uso de Gelocation y Google Maps

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,14 +1,77 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonIcon, IonButton } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { addIcons } from 'ionicons';
+import { location, pin } from 'ionicons/icons';
+import { NgIf } from "@angular/common";
+
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+import { Geolocation } from "@capacitor/geolocation"
+
+import { GoogleMap, Marker } from "@capacitor/google-maps";
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonIcon, IonButton, NgIf],
 })
 export class Tab3Page {
-  constructor() {}
+  @ViewChild('map')
+  mapRef: ElementRef<HTMLElement>;
+  newMap: GoogleMap;
+
+  lugar: string = "";
+
+  latitud: number = 0;
+  longitud: number = 0;
+  constructor() {
+    addIcons({ location, pin });
+    this.obtenerPosicion();
+  }
+
+  async obtenerPosicion(){
+    await Geolocation.getCurrentPosition().then((data) => {
+      this.latitud = data.coords.latitude;
+      this.longitud = data.coords.longitude;
+      console.log(data);
+    })
+    this.createMap();
+  }
+
+  async createMap() {
+    this.newMap = await GoogleMap.create({
+      id: 'my-cool-map',
+      element: this.mapRef.nativeElement,
+      apiKey: environment.apiKey,
+      config: {
+        center: {
+          lat: this.latitud,
+          lng: this.longitud,
+        },
+        zoom: 15,
+      },
+    });
+    this.agregarMarcador();
+  }
+
+  async agregarMarcador(){
+    const marcador: Marker = {
+      coordinate: {
+        lat: this.latitud,
+        lng: this.longitud,
+      },
+      draggable: true
+    }
+    await this.newMap.addMarker(marcador);
+
+    this.newMap.setOnMarkerClickListener(async (marcador) => {
+      this.lugar = `https://www.google.es/maps?q=${marcador.latitude},${marcador.longitude}`;
+      console.log(marcador);
+    })
+  }
 }
