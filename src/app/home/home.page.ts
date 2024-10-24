353f2042b203d66df8cf26c1be9e4931e327b155
avilapro <mejiasavilaeduardo@gmail.com>
2024-10-24 18:31:31 -0400

    clase 1

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,31 @@
+import { NgFor } from '@angular/common';
+import { Component } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonButton, IonIcon } from '@ionic/angular/standalone';
+
+import { addIcons } from "ionicons";
+import { menu, person } from "ionicons/icons";
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  standalone: true,
+  imports: [IonIcon, IonButton, IonButtons, IonHeader, IonToolbar, IonTitle, IonContent, NgFor],
+})
+export class HomePage {
+  productos: any;
+  constructor() {
+    addIcons({menu,person});
+    this.getProducts();
+  }
+
+  getProducts(){
+    fetch('https://fakestoreapi.com/products').
+    then(response => response.json()).
+    then(data => {
+      this.productos = data
+      console.log(this.productos);
+    }) 
+  }
+
+}
