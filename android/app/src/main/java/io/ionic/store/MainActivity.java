9f5805c06a2a8e4515cb575126c56f24e479f100
avilapro <mejiasavilaeduardo@gmail.com>
2024-12-11 22:47:18 -0400

    Configuracion de proyecto android concluido con exito

--- android/app/src/main/java/io/ionic/store/MainActivity.java
+++ android/app/src/main/java/io/ionic/store/MainActivity.java
@@ -1,4 +1,4 @@
-package io.ionic.starter;
+package io.ionic.store;
 
 import com.getcapacitor.BridgeActivity;
 
--- src/app/products/products.page.ts
+++ src/app/products/products.page.ts
@@ -1,29 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonMenuButton, IonMenuToggle, IonIcon } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonMenuButton, IonMenuToggle, IonIcon, IonButton } from '@ionic/angular/standalone';
 
 import { MenuController } from "@ionic/angular";
 
 import { addIcons } from 'ionicons';
-import { homeOutline, receiptOutline, menu } from 'ionicons/icons';
+import { homeOutline, receiptOutline, menu, flashlight } from 'ionicons/icons';
 
 import { Swiper } from "swiper";
+
+// import { CapacitorFlash, CapacitorFlashPlugin } from '@capgo/capacitor-flash'
+import { Flashlight } from "@awesome-cordova-plugins/flashlight/ngx";
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.page.html',
   styleUrls: ['./products.page.scss'],
+  providers: [Flashlight],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonMenuButton, IonMenuToggle, IonIcon]
+  imports: [IonButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonMenuButton, IonMenuToggle, IonIcon]
 })
 export class ProductsPage implements OnInit {
+  luz: boolean = false;
 
-  constructor(private menuCtrl: MenuController) {
-    addIcons({homeOutline,receiptOutline,menu});
+  constructor(private menuCtrl: MenuController, public linterna: Flashlight) {
+    addIcons({menu,flashlight,homeOutline,receiptOutline});
    }
 
-   
+  encenderLuz(){
+    this.luz = !this.luz;
+
+    if (this.luz) {
+      this.linterna.switchOn();
+      // CapacitorFlash.switchOn({});
+    } else {
+      this.linterna.switchOff();
+      // CapacitorFlash.switchOff();
+    }
+  }
+
 
   ngOnInit() {
   }
